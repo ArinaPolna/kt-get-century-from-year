68916cda4a56898b5dc6abfb78202f3c5355da1e
polna <polna.arina@gmail.com>
2026-07-09 19:31:39 +0200

    implement getCentury function

--- src/main/kotlin/mate/academy/GetCentury.kt
+++ src/main/kotlin/mate/academy/GetCentury.kt
@@ -1,5 +1,12 @@
 package mate.academy
 
+const val CENTURY_DELIMITER = 100
+const val ZERO = 0
+const val ONE = 1
+
 fun getCentury(year: Int) : Int {
-    return 0
+    if (year % CENTURY_DELIMITER > ZERO) {
+        return (year / CENTURY_DELIMITER) + ONE
+    }
+    return year / CENTURY_DELIMITER
 }
